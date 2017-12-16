2395f34ebdc66e974a1cb3912bcb54647109f456
khaitun1998 <khaitun1998@gmail.com>
2017-12-16 18:57:55 +0700

    final

---
+++ test/Check.java
@@ -0,0 +1,30 @@
+/**
+ * Created by VAIO on 27-Oct-17.
+ */
+public class Check {
+    public static void main(String[] args) {
+        Rectangle rectangle = new Rectangle();
+        Triangle triangle = new Triangle();
+        Circle circle = new Circle();
+        Square square = new Square();
+        Layer layer = new Layer();
+        Diagram diagram = new Diagram();
+
+        layer.addShape(rectangle);
+        layer.addShape(triangle);
+        layer.addShape(square);
+        layer.addShape(circle);
+
+        diagram.setLayer(layer);
+
+        if (layer.doRemainTriangle()) {
+            layer.deleteTriangle();
+            System.out.println(layer.doRemainTriangle());
+        }
+
+        if (diagram.getLayer().doRemainCircle()) {
+            diagram.deleteCircle();
+            System.out.println(diagram.getLayer().doRemainCircle());
+        }
+    }
+}
